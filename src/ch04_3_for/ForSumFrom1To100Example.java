ebf76a5c6f5b13ab0d4ad4535a012ca685f385ca
bitcamp <bitcamp@DESKTOP-7NUJNL8>
2021-07-28 10:17:56 +0900

    modi

---
+++ src/ch04_3_for/ForSumFrom1To100Example.java
@@ -0,0 +1,13 @@
+package ch04_3_for;
+
+public class ForSumFrom1To100Example {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 50; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}

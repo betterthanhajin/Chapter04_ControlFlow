437fc470a1fc5f88c1366fd148b0e2c0be9ffece
bitcamp <bitcamp@DESKTOP-7NUJNL8>
2021-07-28 11:33:13 +0900

    lec

--- src/ch04_3_for/ForSumFrom1To100Example.java
+++ src/ch04_3_for/ForSumFrom1To100Example.java
@@ -3,11 +3,9 @@ package ch04_3_for;
 public class ForSumFrom1To100Example {
 
 	public static void main(String[] args) {
-		
+		//±¸±¸´Ü
 		for(int i = 0; i < 50; i++) {
-			System.out.println(i);
+			System.out.println(i+2);
 		}
-
 	}
-
 }
